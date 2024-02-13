78b0af596c78f6d50be01c0b4cf23095ac12b729
ahmed hassan <uyu365222@gmail.com>
2024-02-13 18:03:57 +0200

    cccc

---
+++ main.js
@@ -0,0 +1,111 @@
+const APP_ID = "95d9785605f743348665240ac1af9dbf";
+const TOKEN =
+  "007eJxTYJDJrZzI5p5y3los7VSd95kfR8s/ZjuclfdK5TWMn60cslOBwdI0xdLcwtTMwDTN3MTY2MTCzMzUyMQgMdkwMc0yJSltU87p1IZARgb+7yIMjFAI4rMw5CZm5jEwAACj2R07";
+const CHANNEL = "main";
+
+const client = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
+
+let localTracks = [];
+let remoteUsers = {};
+
+let joinAndDisplayLocalStream = async () => {
+  client.on("user-published", handleUserJoined);
+
+  client.on("user-left", handleUserLeft);
+
+  let UID = await client.join(APP_ID, CHANNEL, TOKEN, null);
+
+  localTracks = await AgoraRTC.createMicrophoneAndCameraTracks();
+
+  let player = `<div class="video-container m-2 rounded-3" id="user-container-${UID}">
+                        <div class="video-player" id="user-${UID}"></div>
+                  </div>`;
+  document
+    .getElementById("video-streams")
+    .insertAdjacentHTML("beforeend", player);
+
+  localTracks[1].play(`user-${UID}`);
+
+  await client.publish([localTracks[0], localTracks[1]]);
+};
+
+let joinStream = async () => {
+  await joinAndDisplayLocalStream();
+  document.getElementById("join-btn").style.display = "none";
+  document.getElementById("stream-controls").style.display = "flex";
+};
+
+let handleUserJoined = async (user, mediaType) => {
+  remoteUsers[user.uid] = user;
+  await client.subscribe(user, mediaType);
+
+  if (mediaType === "video") {
+    let player = document.getElementById(`user-container-${user.uid}`);
+    if (player != null) {
+      player.remove();
+    }
+
+    player = `<div class="video-container m-2 rounded-3" id="user-container-${user.uid}">
+                        <div class="video-player" id="user-${user.uid}"></div> 
+                 </div>`;
+    document
+      .getElementById("video-streams")
+      .insertAdjacentHTML("beforeend", player);
+
+    user.videoTrack.play(`user-${user.uid}`);
+  }
+
+  if (mediaType === "audio") {
+    user.audioTrack.play();
+  }
+};
+
+let handleUserLeft = async (user) => {
+  delete remoteUsers[user.uid];
+  document.getElementById(`user-container-${user.uid}`).remove();
+};
+
+let leaveAndRemoveLocalStream = async () => {
+  for (let i = 0; localTracks.length > i; i++) {
+    localTracks[i].stop();
+    localTracks[i].close();
+  }
+
+  await client.leave();
+  document.getElementById("join-btn").style.display = "block";
+  document.getElementById("stream-controls").style.display = "none";
+  document.getElementById("video-streams").innerHTML = "";
+};
+
+const toggleMic = async (e) => {
+  if (localTracks[0].muted) {
+    await localTracks[0].setMuted(false);
+    document.getElementById("mic-icon").className =
+      "fa-solid fa-microphone-lines";
+    e.target.style.backgroundColor = "#3b71ca";
+  } else {
+    await localTracks[0].setMuted(true);
+    document.getElementById("mic-icon").className =
+      "fa-solid fa-microphone-lines-slash";
+    e.target.style.backgroundColor = "#EE4B2B";
+  }
+};
+let toggleCamera = async (e) => {
+  if (localTracks[1].muted) {
+    await localTracks[1].setMuted(false);
+    document.getElementById("camera-icon").className =
+      "fa-solid fa-camera-retro";
+    e.target.style.backgroundColor = "#3b71ca";
+  } else {
+    await localTracks[1].setMuted(true);
+    document.getElementById("camera-icon").className = "fa-solid fa-eye-slash";
+    e.target.style.backgroundColor = "#EE4B2B";
+  }
+};
+
+document.getElementById("join-btn").addEventListener("click", joinStream);
+document
+  .getElementById("leave-btn")
+  .addEventListener("click", leaveAndRemoveLocalStream);
+document.getElementById("mic-btn").addEventListener("click", toggleMic);
+document.getElementById("camera-btn").addEventListener("click", toggleCamera);
